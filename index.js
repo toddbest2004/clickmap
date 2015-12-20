c55b33a93e36437a808690ad93b86fc875bad813
Todd Best <toddbest2004@gmail.com>
2015-12-19 17:20:44 -0800

    Basic setup for express/angular

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var db = require('./mongoose');
+var path = require('path')
+var session = require('express-session');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/*', function(req,res){
+	res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+var port = process.env.PORT || 3000;
+var serverip = process.env.IP || "localhost";
+
+app.listen(port, serverip);
+console.log('Server running at '+serverip+":"+port);
---
+++ public/app/contollers.js
@@ -0,0 +1,4 @@
+angular.module('ClickMapCtrls', [])
+.controller('ClickMapCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+
+}])
