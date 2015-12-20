bdff86900e8482b954e066dface39bf31437b8eb
Todd Best <toddbest2004@gmail.com>
2015-12-19 18:45:14 -0800

    Able to add placeholder rectangles in place of images on the map.

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,4 +1,16 @@
 angular.module('ClickMapCtrls', [])
 .controller('ClickMapCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-
+	$scope.colors=["black", "blue", "red", "yellow"]
+	$scope.icons=[]
+	$scope.currentColor=0
+	$scope.svgClick=function(e){
+		$scope.icons.push({x:e.offsetX-4,y:e.offsetY-4,color:$scope.colors[$scope.currentColor]})
+		console.log($scope.icons)
+	}
+	$scope.iconClick=function(e){
+		e.stopPropagation()
+	}
+	$scope.chooseColor=function(index){
+		$scope.currentColor=index
+	}
 }])
