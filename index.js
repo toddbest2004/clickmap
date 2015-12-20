422e5e3af65b53956374f2661c65c1c3479d7840
Todd Best <toddbest2004@gmail.com>
2015-12-19 17:31:34 -0800

    mongoose/angular framework in place.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended:true }));
 
 app.get('/*', function(req,res){
 	res.sendFile(path.join(__dirname, 'public/index.html'))
