6d6c55bdefee6487f9bc31b364916af5a25ddf6b
Todd Best <toddbest2004@gmail.com>
2015-12-29 20:10:20 -0800

    Adding ng-attr- to prevent console errors on load.

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,16 +1,53 @@
 angular.module('ClickMapCtrls', [])
-.controller('ClickMapCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+.controller('ClickMapCtrl', ['$scope', '$http', '$location', '$routeParams', '$timeout', function($scope, $http, $location, $routeParams, $timeout) {
+	$scope.iconSize = 12
 	$scope.colors=["black", "blue", "red", "yellow"]
 	$scope.icons=[]
 	$scope.currentColor=0
+	$scope.editing=false
+	$scope.newElement=null
+	$scope.editTop=0
+	$scope.editLeft=0
 	$scope.svgClick=function(e){
-		$scope.icons.push({x:e.offsetX-4,y:e.offsetY-4,color:$scope.colors[$scope.currentColor]})
-		console.log($scope.icons)
+		$scope.newElement={x:e.offsetX-$scope.iconSize/2,y:e.offsetY-$scope.iconSize/2,color:$scope.colors[$scope.currentColor]}
+		$scope.editing=true
+		$scope.editTop=e.offsetY-$scope.iconSize/2
+		$scope.editLeft=e.offsetX+$scope.iconSize
+		$timeout(function(){
+			//timeout allows for the elements to render before giving focus.
+			document.getElementById("elementTitle").focus()
+		})
 	}
 	$scope.iconClick=function(e){
 		e.stopPropagation()
+		//follow link if link
 	}
-	$scope.chooseColor=function(index){
+	$scope.chooseIcon=function(index){
 		$scope.currentColor=index
+	}
+	$scope.mouseIn=function(index){
+		$scope.hover=$scope.icons[index]
+	}
+	$scope.mouseOut=function(index){
+		$scope.hover=null
+	}
+	$scope.save=function(){
+		if(!$scope.newElement.title){
+			$scope.newElement.title=''
+		}
+		$scope.icons.push($scope.newElement)
+		$scope.editing=false
+		$scope.newElement=null
+	}
+	$scope.cancel=function(){
+		$scope.editing=false
+		$scope.newElement=null
+	}
+	$scope.keypress=function(keyEvent){
+		if(keyEvent.keyCode==13){
+			$scope.save()
+		}else if(keyEvent.keyCode===27){
+			$scope.cancel()
+		}
 	}
 }])
